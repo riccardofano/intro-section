93e1cfe412f7484fb0d387db46e301c786164c2f
riccardofano <rcr.fano@gmail.com>
2022-07-16 15:59:53 +0200

    add ability to close nav by clicking outside it

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
 const navToggle = document.querySelector(".nav__toggle");
 const navList = document.querySelector(".nav");
+const overlay = document.querySelector(".overlay");
 const subLists = document.querySelectorAll(".nav__sublist");
 
+// Open/Close mobile navbar by clicking on toggle button
 navToggle.addEventListener("click", function () {
   const expanded = this.getAttribute("aria-expanded") === "true";
   navList.setAttribute("data-active", !expanded);
   this.setAttribute("aria-expanded", !expanded);
 });
 
+// Close mobile navbar by clicking outside it
+overlay.addEventListener("click", function () {
+  navList.setAttribute("data-active", false);
+  navToggle.setAttribute("aria-expanded", false);
+});
+
+// Open/Close sublist by clicking on the parent li
 for (const list of subLists) {
   list.parentElement.addEventListener("click", function (e) {
     if (e.target !== this) return;
